18be94292236f92dbf3918901235533834b62477
Gleb Motuz <hihoho@mail.ru>
2016-12-16 20:15:16 +0300

    work-2

---
+++ js/slick.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    initialSlide: 0,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    // autoplay: true,
+    // autoplaySpeed: 5500,
+    // arrows: false,
+    // speed: 1500,
+
+    // responsive: [
+    // {
+    //   breakpoint: 760,
+    //   settings: {
+    //     arrows: true
+    //   }
+    // }
+    // ]
+  });
+});
